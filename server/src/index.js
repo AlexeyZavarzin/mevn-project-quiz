5be2a7c47d6f8a70682efb54c449a077f5b14cb1
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-01 13:53:12 +0400

    server backbone

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello  World")
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const config = require('./config/config')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT || config.port,
+    () => console.log('Server start on port ${config.port}...'))
