6f8e7527fb6b8a1aa20ce168a789907ed1310716
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-06 21:30:48 +0400

    some some

--- server/src/app.js
+++ server/src/app.js
@@ -7,9 +7,20 @@ const config = require('./config/config')
 
 const app = express()
 
+
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.listen(process.env.PORT || config.port,
     () => console.log(`Server start on port ${config.port}...`))
+
+app.get('/posts', (req, res) => {
+    res.send(
+        [{
+            title: "Hello World!",
+            description: "Hi there! How are you?"
+        }]
+    )
+})
