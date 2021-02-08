bb804f85c45f4910f5a82e3664855fe01cb83401
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-08 23:52:31 +0400

    mongodb

--- server/src/mongobdConnection.js
+++ server/src/mongobdConnection.js
@@ -1,2 +1,14 @@
-var mongoose = require('mongoose')
+import config from "./config/config";
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(config.dbURL, config.dbOptions)
+mongoose.connection
+    .once('open', () => {
+        console.log(`Mongoose - successful connection ...`)
+        app.listen(process.env.PORT || config.port,
+            () => console.log(`Server start on port ${config.port} ...`))
+    })
+    .on('error', error => console.warn(error))
