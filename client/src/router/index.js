0e30b846811b06e5cf812d2b5e5214d6eab24bab
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-09 21:19:15 +0400

    create and read operations

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Posts from '../components/Posts'
+import NewPost from '../components/NewPost'
 
 Vue.use(Router)
 
@@ -11,6 +13,11 @@ export default new Router({
       path: '/posts',
       name: 'Posts',
       component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
     }
   ]
 })
--- client/src/services/PostsService.js
+++ client/src/services/PostsService.js
@@ -2,6 +2,10 @@ import Api from './Api'
 
 export default {
   fetchPosts() {
-    return Api().get('posts')
+    return Api().get('posts');
+  },
+
+  addPost(params) {
+    return Api.post('posts', params);
   }
 }
