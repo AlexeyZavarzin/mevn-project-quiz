f53c84368f32f58c1c544650572e31873ee13e47
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-15 15:32:45 +0400

    consts and .then()

--- server/src/app.js
+++ server/src/app.js
@@ -13,14 +13,12 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-mongoose.connect('mongodb://localhost:27017/posts')
-var db = mongoose.connection
-db.on("error", console.error.bind(console, "connection error"))
-db.once("open", function(callback) {
-    console.log("Connection Succeeded")
-})
+mongoose.connect('mongodb://localhost:27017/posts').then(() => console.log("Successful database connection"))
+const db = mongoose.connection;
 
-// Fetch all posts
+db.on("error", console.error.bind(console, "connection error"))
+db.once("open", () => console.log("Connection Succeeded"))
+
 app.get('/posts', (req, res) => {
     Post.find({}, 'title description', function (error, posts) {
         if (error) {
@@ -32,11 +30,10 @@ app.get('/posts', (req, res) => {
     }).sort({_id:-1})
 })
 
-// Add new post
 app.post('/add_post', (req, res) => {
-    var title = req.body.title
-    var description = req.body.description
-    var new_post = new Post({
+    const title = req.body.title
+    const description = req.body.description
+    const new_post = new Post({
         title: title,
         description: description
     })
